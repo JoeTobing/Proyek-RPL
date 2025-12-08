216cd7bf7ffc9334eb828ee2d1848c2ea36ae5c3
Jonathan <tobingkagum29@gmail.com>
2025-12-08 19:52:08 +0700

    Update server.js

--- backend-servis/server.js
+++ backend-servis/server.js
@@ -16,7 +16,7 @@ app.use('/api/auth', authRoutes);
 
 // 🌐 Port dan Host
 const PORT = process.env.PORT || 4000;
-const HOST = '127.0.0.1';
+const HOST = '0.0.0.0';
 
 // ▶️ Jalankan server
 app.listen(PORT, HOST, () => {
